48b80b63089e79068fdaeb94e33cd671d3970a20
chakoch <sam.jaudat@gmail.com>
2025-10-24 12:37:44 +0200

    Refactor SystemMonitor class

--- monitor.py
+++ monitor.py
@@ -1,41 +1,21 @@
-"""
-Monitor-modul för systemövervakning
-Hanterar CPU, minne och diskanvändning
-"""
-
 import psutil
 
 class SystemMonitor:
-    """Klass för att övervaka systemresurser"""
     
     def __init__(self):
-        """Initierar systemövervakaren"""
         self.is_monitoring = False
     
     def start_monitoring(self):
-        """Startar övervakning"""
         self.is_monitoring = True
     
     def stop_monitoring(self):
-        """Stoppar övervakning"""
         self.is_monitoring = False
     
     def get_cpu_usage(self):
-        """
-        Hämtar CPU-användning i procent
-        
-        Returns:
-            float: CPU-användning i procent
-        """
         return psutil.cpu_percent(interval=1)
     
     def get_memory_usage(self):
-        """
-        Hämtar minnesanvändning
-        
-        Returns:
-            dict: Minnesstatistik med total, använt, procent
-        """
+
         memory = psutil.virtual_memory()
         return {
             'total': memory.total / (1024 ** 3),  # GB
@@ -44,12 +24,7 @@ class SystemMonitor:
         }
     
     def get_disk_usage(self):
-        """
-        Hämtar diskanvändning
-        
-        Returns:
-            dict: Diskstatistik med total, använt, procent
-        """
+
         disk = psutil.disk_usage('/')
         return {
             'total': disk.total / (1024 ** 3),    # GB
@@ -58,12 +33,7 @@ class SystemMonitor:
         }
     
     def get_current_stats(self):
-        """
-        Hämtar alla aktuella systemstatistik
-        
-        Returns:
-            dict: Komplett systemstatistik
-        """
+
         memory = self.get_memory_usage()
         disk = self.get_disk_usage()
         

d92980287ed2e917daccf047da14b5e145081a08
chakoch <sam.jaudat@gmail.com>
2025-10-24 09:35:42 +0200

    Add constants.py (centrala mappningar/labels)

---
+++ constants.py
@@ -0,0 +1,28 @@
+
+ALARM_TYPE_NAMES = {
+    'CPU': 'CPU larm',
+    'Memory': 'Minneslarm',
+    'Disk': 'Disklarm'
+}
+
+ALARM_TYPE_WARNINGS = {
+    'CPU': 'CPU ANVÄNDNING',
+    'Memory': 'MINNESANVÄNDNING',
+    'Disk': 'DISKANVÄNDNING'
+}
+
+MENU_TO_ALARM_TYPE = {
+    '1': 'CPU',
+    '2': 'Memory',
+    '3': 'Disk'
+}
+
+MENU_TO_ALARM_TYPE_SWEDISH = {
+    '1': 'CPU användning',
+    '2': 'Minnesanvändning',
+    '3': 'Diskanvändning'
+}
+
+# Formattering
+SEPARATOR_LENGTH = 50
+SEPARATOR_CHAR = '='
